225b6230aaa1e634d4dac803de334537209a2ada
joymush <joy.chemutai@student.moringaschool.com>
2022-08-27 08:21:08 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+const { getModulesPluginNames } = require("@babel/preset-env")
+
+let cards = ["Guadalupe", "Ollie", "Aki"]
+
+function writeCards(names, suprise){
+    let newArr = []
+    names.forEach(element => {
+        newArr.push(`Thank you, ${element}, for the wonderful ${suprise} gift!`)
+    });
+    return newArr
+}
+ function countDown(number){
+    for(let i = 0; i <= number; i++){
+        console.log(i)
+    }
+ }
